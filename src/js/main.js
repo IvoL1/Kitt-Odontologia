93132b6ee8408f4150a1daccafa488646c827b5e
Ivo Luis <ivoluis239@gmail.com>
2026-05-05 22:12:37 -0300

    :sparkles: feat: add click event to menu toggle items for better navigation

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,19 @@ const navList = document.querySelector('.header__nav-list');
 
 const menuToggle = document.querySelector('.header__menu-toggle');
 
+const menuToggleItems = document.querySelectorAll('.header__nav-list a');
+
 function toggleMenu() {
   navList.classList.toggle('open');
   menuToggle.classList.toggle('is-open');
 }
 
 menuToggle.addEventListener('click', toggleMenu);
+
+menuToggleItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (navList.classList.contains('open')) {
+      toggleMenu();
+    }
+  });
+});
